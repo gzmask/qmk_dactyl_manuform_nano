48c32b2f344dc8935fab68a69f494c6fde05ef10
ray-lei <r.lei@sap.com>
2019-01-23 22:43:47 -0800

    increase serial com speed.

--- keyboards/handwired/dactyl_manuform_nano/config.h
+++ keyboards/handwired/dactyl_manuform_nano/config.h
@@ -40,6 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* serial.c configuration for split keyboard */
 #define SOFT_SERIAL_PIN D0
+#define SELECT_SOFT_SERIAL_SPEED 0 /* 1:default, 2, 3, 4, 5 */
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
