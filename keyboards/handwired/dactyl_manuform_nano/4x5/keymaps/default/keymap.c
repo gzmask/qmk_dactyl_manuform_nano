e4ee03bb350893d43dc9ef4f2d3904950576fd1d
ray-lei <r.lei@sap.com>
2019-12-30 10:29:36 -0800

    Better control alt combo.

--- keyboards/handwired/dactyl_manuform_nano/4x5/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform_nano/4x5/keymaps/default/keymap.c
@@ -47,26 +47,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------,                             ,----------------------------------,
  * |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |  p   |
  * |------+------+------+------+------|                             |-------------+------+------+------|
- * |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  | ;    |
+ * | CTL a|   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  | ;    |
  * |------+------+------+------+------|                             |------|------+------+------+------|
  * |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  | /    |
  * |------+------+------+-------------,                             ,-------------+------+------+------,
- *        | ALT =|  -   |                                                         |   [  |   ]  |
+ *        |  =   |  -   |                                                         |   [  |   ]  |
  *        '------+------'-------------'------+------' '------+------'-------------'------+------'
- *                             |SFTSPC|CTLTAB|RAIENT| |RAIENT|CTLBAC|SFTSPC|
+ *                             |SFTSPC|CTLTAB|RAIENT| |RAIBAC|ALTENT|SFTSPC|
  *                             +------|------+------' '------+------|------+
  *                                    |GUIESC|LOWgrv| |LOWquo|GUI \ |
  *                                    +------'------+ +------'------+
  */
 
 [_BASE] = LAYOUT( \
-  KC_Q, KC_W, KC_E, KC_R, KC_T,                                           KC_Y, KC_U, KC_I,    KC_O,   KC_P,          \
-  KC_A, KC_S, KC_D, KC_F, KC_G,                                           KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,       \
-  KC_Z, KC_X, KC_C, KC_V, KC_B,                                           KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
-        ALT_T(KC_EQL), KC_MINS,                                                       KC_LBRC, KC_RBRC,               \
-                       SFT_SPC,                                           SFT_SPC,                                    \
-                CTL_T(KC_TAB), LT(_RAISE, KC_DEL),         LT(_RAISE, KC_BSPC), CTL_T(KC_ENT), \
-                GUI_T(KC_ESC), LT(_LOWER, KC_GRV),         LT(_LOWER, KC_QUOT), GUI_T(KC_BSLS)
+  KC_Q, KC_W,  KC_E, KC_R, KC_T,                                           KC_Y, KC_U, KC_I,    KC_O,   KC_P,    \
+  CTL_T(KC_A), KC_S, KC_D, KC_F, KC_G,                                     KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, \
+  KC_Z,        KC_X, KC_C, KC_V, KC_B,                                     KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
+               KC_EQL, KC_MINS,                                            KC_LBRC, KC_RBRC,                     \
+                       SFT_SPC,                                            SFT_SPC,                              \
+               CTL_T(KC_TAB), LT(_RAISE, KC_DEL),         LT(_RAISE, KC_BSPC), ALT_T(KC_ENT),                    \
+               GUI_T(KC_ESC), LT(_LOWER, KC_GRV),         LT(_LOWER, KC_QUOT), GUI_T(KC_BSLS)
 ),
 
 /* Raise
